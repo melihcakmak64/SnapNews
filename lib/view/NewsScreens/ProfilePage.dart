f54c270ad75d51d299b1fab76a601f18c0050701
Mehmet Anıl <m.cmert1907@hotmail.com>
2024-05-02 03:04:42 +0300

    X logo eklendi

--- lib/view/NewsScreens/ProfilePage.dart
+++ lib/view/NewsScreens/ProfilePage.dart
@@ -57,7 +57,7 @@ class _SettingsScreen2 extends State<MainSettingsScreen> {
                       name: 'X',
                       leadingIcon: Padding(
                         padding: const EdgeInsets.only(left: 1.0),
-                        child: Image.asset("resimler/x_logo.jpg",
+                        child: Image.asset("images/x_logo.jpg",
                             height: 30, fit: BoxFit.cover),
                       ),
                     ),
